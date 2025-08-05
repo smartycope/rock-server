908eddf8c4d20436a34be320897f3c9512bdde8f
Copeland Carter <smartycope@gmail.com>
2025-08-05 00:49:52 -0500

    Added endpoints info and install

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import subprocess
 import git
 import logging
 import io
+import psutil
+import os
+import time
 
 # Custom logging, since it's not super accessible using gunicorn
-log_stream = io.StringIO()
+log_stream = io.StringIO('Server started')
 handler = logging.StreamHandler(log_stream)
 log = logging.getLogger("my_logger")
 log.setLevel(logging.INFO)
@@ -13,6 +16,7 @@ log.addHandler(handler)
 
 SERVICE_NAME = "rock-server"
 repo = git.Repo("/home/rock/rock-server")
+python_binary = "/home/rock/rock-server/bin/python"
 app = Flask(__name__)
 
 @app.route("/")
@@ -50,7 +54,40 @@ def github_webhook():
 def logs():
     return log_stream.getvalue()
 
+@app.route("/info")
+def info():
+    proc = psutil.Process(os.getpid())
+    start_time = proc.create_time()
+    uptime_seconds = time.time() - start_time
+    return jsonify(
+        {
+            "status": "ok",
+            "server_started": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)),
+            "uptime": uptime_seconds,
+            "uptime_human": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(uptime_seconds)),
+            "current_packages": subprocess.check_output([python_binary, "-m", "pip", "freeze"]).decode("utf-8").split("\n"),
+            "logs": log_stream.getvalue()
+        }
+    ), 200
+
+@app.route("/install/<package>", methods=["POST"])
+def install_package(package):
+    """ Install a package using pip based on the package name """
+    try:
+        log.info("Installing %s...", package)
+        subprocess.run([python_binary, "-m", "pip", "install", package], check=True)
+        log.info("%s installed", package)
+    except Exception as e:
+        log.error("Failed to install %s: %s", package, e)
+        return jsonify({"error": str(e)}), 500
+    return jsonify({"status": "ok"}), 200
+
+# Log all requests
+@app.before_request
+def log_request_info():
+    log.info("Request: %s %s", request.method, request.url)
+
 if __name__ == "__main__":
-    log.info("Server started")
+    log.info("Server started via main")
     # Gunicorn will handles the server port and IP
     app.run()
